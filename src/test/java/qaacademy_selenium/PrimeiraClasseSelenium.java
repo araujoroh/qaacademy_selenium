238cd5a7d53b43267601935d9bddd1f0badb302b
Robson Reis <robsonra.rs@gmail.com>
2021-07-03 10:42:41 -0300

    Exercicios

--- src/test/java/qaacademy_selenium/PrimeiraClasseSelenium.java
+++ src/test/java/qaacademy_selenium/PrimeiraClasseSelenium.java
@@ -8,7 +8,7 @@ public class PrimeiraClasseSelenium {
 	public void testeLogin() {
 		
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.com.br/ref=nav_logo");
+		driver.get("https://www.americanas.com.br/?epar=bp_br_00_go_sch_brand_americanas_202002&utm_medium=buscappc&utm_source=google&utm_campaign=marca:acom%3bmidia:buscappc%3bformato:branding%3bsubformato:00%3bidcampanha:sch_brand_americanas_202002&opn=YZMEZP&WT.srch=1&gclid=CjwKCAjwz_WGBhA1EiwAUAxIcdT0_pYqx3HHW3wec0oXPDRkDdzPx6SHp2gAaDaXFbOc8udInH1hbBoCndoQAvD_BwE");
 		
 	}
 	
